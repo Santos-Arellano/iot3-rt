50aa2dc7bed208366894c8e4ca88f1bbc4808f58
Santos Alejandro Arellano Olarte <arellanosantoso6@gmail.com>
2023-11-10 14:43:29 +0000

    firebase try 1

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,38 +1,54 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("../../serviceAccountKey.json");
+const admin = require("firebase-admin");
+const serviceAccount = require("../../serviceAccountKey.json");
+const express = require('express');
+const router = express.Router();
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://iot2023-5d518-default-rtdb.firebaseio.com"
+  databaseURL: "https://iot2023-32f35-default-rtdb.firebaseio.com/"
 });
 
 const db = admin.database();
 
-const { Router}= require('express');
-const router = Router();
-
-router.get('/', (req, res) => {
-    db.ref('contacts').once('value', (snapshot) => {
-       data = snapshot.val();
-       res.render('index', {contacts: data})
-    });
-})
-
-router.post('/new-contact', (req, res) => {
-    const newContact = {
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        email: req.body.email,
-        phone: req.body.phone
+// Ruta para mostrar todos los contactos
+router.get('/', async (req, res) => {
+    try {
+        const snapshot = await db.ref('contacts').once('value');
+        const data = snapshot.val();
+        res.render('index', { contacts: data });
+    } catch (error) {
+        console.error("Error fetching contacts:", error);
+        res.status(500).send("Internal Server Error");
     }
+});
+
+// Ruta para agregar un nuevo contacto
+router.post('/new-contact', (req, res) => {
+    const { firstname, lastname, email, phone } = req.body;
+
+    const newContact = {
+        firstname,
+        lastname,
+        email,
+        phone
+    };
+
     db.ref('contacts').push(newContact);
     res.redirect('/');
 });
 
+// Ruta para eliminar un contacto
 router.get('/delete-contact/:id', (req, res) => {
-    db.ref('contacts/' + req.params.id).remove();
-    res.redirect('/');
+    const contactId = req.params.id;
+
+    db.ref(`contacts/${contactId}`).remove()
+        .then(() => {
+            res.redirect('/');
+        })
+        .catch((error) => {
+            console.error("Error deleting contact:", error);
+            res.status(500).send("Internal Server Error");
+        });
 });
 
 module.exports = router;
